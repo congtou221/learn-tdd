c8971143a062676c080076257c564a340de0dc15
sixiaohui <nongmeimao123>
2016-06-08 16:50:27 +0800

    [learn TDD] add defauts result to parse function

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -3,7 +3,27 @@ var tags = require('../lib/tags');
 
 describe('Tags', function(){
 	describe('#parse()', function(){
-		
+		it('should have long formed tags', function(){
+			var args = ['--depth=4', '--hello=world'];
+			var result = tags.parse(args);
+			
+			expect(result).to.have.a.property('depth', 4);
+			expect(result).to.have.a.property('hello', 'world');			
+		})
+		it('should fallback to defaults', function(){
+			var args = ['--depth=4', '--hello=world'];
+			var defaults = {depth: 2, foo: 'bar'};
+			var result = tags.parse(args, defaults);
+
+			var expected = {
+				depth: 4,
+				hello: 'world',
+				foo: 'bar'
+			}
+
+			expect(result).to.deep.equal(expected);
+		})
+
 	})
 
 })
